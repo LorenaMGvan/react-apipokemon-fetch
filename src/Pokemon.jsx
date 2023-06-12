f26c72ad79a5d75b48e6b7e3fbe4454e68717565
LorenaMGvan <lorena.mgvan@hotmail.com>
2023-06-12 16:52:27 -0600

    add pager

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -3,30 +3,38 @@ import React, { useState, useEffect } from "react";
 export const Pokemon = () => {
   // creamons variables de estado para guardar pokemones
   const [pokemones, setPokemones] = useState([]);
+  const [previous, setPrevious] = useState(null);
+  const [next, setNext] = useState(null);
+  const [current, setCurrent] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=5&offset=0"
+  );
 
   // usamons useEffect para hacer el llamado a la API
   // se ejecuta al inicio y cada que cambie un valor
-  useEffect(() => {
-    async function getPokemones() {
-      const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon?limit=5&offset=0",
-        []
-      );
-      const data = await response.json();
-      setPokemones(data.results);
-    }
-    getPokemones();
-  });
+  async function getPokemones() {
+    const response = await fetch(current, []);
+    const data = await response.json();
+    console.log(data.results);
+    setPokemones(data.results);
+    setPrevious(data.previous);
+    setNext(data.next);
+  }
 
+  useEffect(() => {
+    getPokemones();
+  }, [current]);
+  
   return (
     <>
       <ul>
         {pokemones.map((item, index) => (
-          <li key={index}>{item.name}</li>
+          <li key={index}>
+            {item.name}
+        </li>
         ))}
       </ul>
-      <button>Anterior</button>
-      <button>Siguiente</button>
+      <button onClick={() => previous !== null && setCurrent(previous)}>Anterior</button>
+      <button onClick={() => next !== null && setCurrent(next) }>Siguiente</button>
     </>
   );
 };
